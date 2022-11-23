891a8d9d950609f11fe805ca6cfb25d9db5f2433
poletaev-petr <poletaev.petr@gmail.com>
2022-11-23 18:09:44 +0300

    fix empty result

--- wordcount.go
+++ wordcount.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
-	v := strings.Split(os.Args[1], " ")
-	fmt.Println(len(v))
+	if len(os.Args) == 2 && os.Args[0] != " " && os.Args[0] != "" &&
+		os.Args[1] != " " && os.Args[1] != "" {
+		v := strings.Split(os.Args[1], " ")
+		fmt.Println(len(v))
+
+	} else {
+		fmt.Println("0")
+	}
+
 }
